de3551583bdb2451a414a2b600fb1bfb41d9a731
MominaAmerAkhter <22100009@lums.edu.pk>
2023-08-02 17:54:44 +0500

    outpatient screen

--- medics-frontend/src/OutPatient.js
+++ medics-frontend/src/OutPatient.js
@@ -2,21 +2,94 @@ import React from "react"
 import { useState } from "react";
 import validator from 'validator';
 import './App.css';
-import { useNavigate } from "react-router-dom"
-// import Button from 'react-bootstrap/Button';
+import { Link, useNavigate } from "react-router-dom"
 import NavAdmin from "./NavAdmin";
+import Card from 'react-bootstrap/Card';
+import OutPatient from './OutPatient';
+import outPatient from "./outpatient.png";
+import shifts from "./shifts.png";
+import payments from "./payments.png";
+import outpatient from "./outpatient_med.png";
+import prescriptions from "./prescriptions.png";
+import appointments from "./appointments.png";
+import reports from "./reports.png";
 
-const OutPatient = () => {
 
-
+const HomeScreen = () => {
+                                                                                                                                                                                                                                                              
     return (
-        <div>
+        <div >
             <NavAdmin/>
-            Out patient
+            <div className="Outpatient-body">
+                <div className="Panel-cards-double">
+                    <div className="Panel-cards-single">
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={shifts} alt="shifts" />
+                                    <Card.Title>MANAGE SHIFTS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="Panel-cards-single">           
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={prescriptions} alt="prescriptions" />
+                                    <Card.Title>OPD PRESCRIPTIONS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>     
+                    </div>
+                </div>
+                <div className="Panel-cards-double">
+                    <div className="Panel-cards-single">
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={appointments} alt="appointments" />
+                                    <Card.Title>APPOINTMENTS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="Panel-cards-single">
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={payments} alt="payments" />
+                                    <Card.Title>PAYMENTS & REFUNDS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>
+                <div className="Panel-cards-double">
+                    <div className="Panel-cards-single">
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={outpatient} alt="outpatient" />
+                                    <Card.Title>OUT PATIENTS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </div>
+                    <div className="Panel-cards-single">
+                        <Link to='/OutPatient' className="link-cards">
+                            <Card className="card">
+                                <Card.Body>
+                                    <Card.Img variant="centre" src={reports} alt="reports" />
+                                    <Card.Title>REPORTS</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 } 
 
-
-
-export default OutPatient;
+export default HomeScreen;
